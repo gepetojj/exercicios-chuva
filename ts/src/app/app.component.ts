bd998d6317a1f6f5f27bb082c1771f9be1e03276
João Pedro <nmjoaopedro22@gmail.com>
2024-04-02 16:25:13 -0300

    estrutura esqueleto da página

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DevChuva';
+  title =
+    'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP';
 }
