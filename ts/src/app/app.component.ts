16ea4bfeea00fcf65a3b2fc3cec9eb394bd6e22e
João Pedro <nmjoaopedro22@gmail.com>
2024-04-03 21:38:37 -0300

    adiciona lógica do painel de discussões

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+type CommentMode = 'waiting' | 'creating' | 'created';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +10,13 @@ export class AppComponent {
   title =
     'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP';
   showingMore = false;
+  commentMode: CommentMode = 'waiting';
 
   toggleShowMore() {
     this.showingMore = !this.showingMore;
+  }
+
+  changeCommentMode(mode: CommentMode) {
+    this.commentMode = mode;
   }
 }
